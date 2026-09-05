e820b700d512e9aaaadae3545800466183ba4a95
Novaro Digital Labs <murimi.2330@gmail.com>
2026-09-05 19:05:23 +0300

    fix firebase Auth exception import

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
